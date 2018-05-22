d355d9f9bbf636ab75a560e73657fcacfa067746
Ramón Casaña <racamar@campusaula.com>
2018-05-22 15:36:06 +0200

    Añadido ajustes

---
+++ src/pages/ajustes/ajustes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AjustesPage } from './ajustes';
+
+@NgModule({
+  declarations: [
+    AjustesPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AjustesPage),
+  ],
+})
+export class AjustesPageModule {}
---
+++ src/pages/ajustes/ajustes.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+import { IonicPage, LoadingController, NavController, NavParams,AlertController, MenuController } from 'ionic-angular';
+import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFireDatabase } from "angularfire2/database";
+import { MapsPage } from '../maps/maps';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { LoginPage } from '../login/login';
+
+@IonicPage()
+
+@Component({
+  selector: 'page-ajustes',
+  templateUrl: 'ajustes.html',
+  providers: [GooglePlus]
+})
+export class AjustesPage {
+
+  actualiza: string;
+  name: string;
+  password: string;
+  email:string;
+  surname :string;
+  country : string;
+
+  constructor(public navCtrl: NavController,
+              private fire:AngularFireAuth,
+              private alertCtrl: AlertController,
+              public navParams: NavParams,
+              public aut: AngularFireAuth,
+              public db: AngularFireDatabase,
+              private googlePlus: GooglePlus,
+              public menu: MenuController,
+              public loadingCtrl: LoadingController) {
+
+    this.email = aut.auth.currentUser.email;
+    this.menu.enable(true);
+
+  }
+
+  showSearch() {
+    this.navCtrl.setRoot(MapsPage);
+  }
+
+  enviarpass(){
+    return this.fire.auth.sendPasswordResetEmail(this.email)
+      .then(data => {
+        this.alertReset(data);
+
+      });
+  }
+  alertReset(message: string) {
+
+    this.alertCtrl.create({
+      // el titulo de la ventana
+      title: 'Look your email',
+      // el (message) es lo que recoge tanto el error como el verdadero
+      subTitle: message,
+      // el boton para quitarlo
+      buttons: ['OK']
+    }).present();
+  }
+
+  enviar() {
+
+    this.db.database.ref('usuario/' + this.email.split(".")).set({
+
+      name: this.name,
+      surname: this.surname,
+      country:this.country,
+
+    });
+
+
+    this.loadingCtrl.create({
+      // el texto que saldra
+      content: 'Enviando Información',
+      // duración
+      duration: 3000,
+      // que sea visible
+      dismissOnPageChange: true
+
+    }).present();
+  }
+
+
+  async logout(): Promise<any> {
+    if (this.aut.auth.currentUser != null) {
+      this.navCtrl.setRoot(LoginPage);
+      this.googlePlus.logout();
+      return this.aut.auth.signOut();
+    }
+  }
+}
